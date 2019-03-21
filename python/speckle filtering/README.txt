c31371b436115ae086b82b054c18eb22162e4856
kvngvikram <happykvng@gmail.com>
2019-03-21 18:24:13 +0530

    noise added

---
+++ python/speckle filtering/README.txt
@@ -0,0 +1,7 @@
+This is for the internal assigment for Microwave remote sensing course. Original image will be modified with speckles using matlab. 
+speckle filtering will be tried. 
+Photo by Rober González on Unsplash
+
+Links refered : 
+https://stackoverflow.com/questions/22937589/how-to-add-noise-gaussian-salt-and-pepper-etc-to-image-in-python-with-opencv
+
---
+++ python/speckle filtering/add_salt_pepper_noise.py
@@ -0,0 +1,28 @@
+import numpy as np 
+import random
+import cv2 
+
+
+def sp_noise(image,prob,low_value,high_value):   # function to add salt and pepper noise to image 
+
+    output = np.zeros(image.shape, np.uint8)
+    thres = 1 - prob
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rnd = random.random()
+            if rnd < prob:
+                output[i][j] = low_value
+            elif rnd > thres:
+                output[i][j] = high_value
+            else:
+                output[i][j] = image[i][j]
+
+    return output
+
+
+image = cv2.imread('original.jpg',0)
+noise_image = sp_noise(image,0.05,5,250)
+cv2.imwrite('noise_image.png',noise_image)
+
+
