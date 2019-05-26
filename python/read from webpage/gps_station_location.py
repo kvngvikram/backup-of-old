c09dd5780e87d952c7c49daaedc536611da4c31c
kvngvikram <happykvng@gmail.com>
2019-05-26 07:55:29 +0530

    able to get the lat as string

--- python/read from webpage/gps_station_location.py
+++ python/read from webpage/gps_station_location.py
@@ -11,9 +11,15 @@ print('connection successful' if page.status_code == 200 else 'connection failed
 a = str(page.text)
 print(type(a))
 
-lati = a.find('Latitude')
-loni = a.find('Longitude')
+lat_index = a.find('Latitude')       # Index for latitude
+lon_index = a.find('Longitude')      # index for longitude
 
-print(a[lati-6:lati+25])
-print(a[loni-6:loni+25])
+lat_string = a[lat_index+9:lat_index+70]
+lon_string = a[lon_index+10:lon_index+40]
 
+print(lat_string)
+
+end_string = '</h4>'
+
+lat_string = lat_string[:lat_string.find(end_string)]
+print(lat_string)
