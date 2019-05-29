c5bcbf09b13a0a153c8adc61eb08a5b2901a8e32
Vikram <vikram@conrad.anu.edu.au>
2019-05-29 14:04:21 +1000

    just saved them

--- python/data_extraction/try.py
+++ python/data_extraction/try.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import numpy as np
 
 filename = 'data.txt'
 mywidths = [8, 5, 3, 19, 19, 11, 11, 14, 14]
 data = pd.read_fwf(filename,widths=mywidths,header=None,skiprows=3)
 print(data)
+
+npdata = np.asarray(data)
+print(npdata)
+print(npdata[:,1:7])
+np.savetxt('temp.dat',npdata[:,1:7],delimiter=' ')
