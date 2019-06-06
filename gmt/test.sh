91cc419b754c09059621a4c0d568898a169097fa
Vikram <vikram@conrad.anu.edu.au>
2019-06-06 16:52:42 +1000

    able to do some GMT

--- gmt/test.sh
+++ gmt/test.sh
@@ -1,13 +1,19 @@
 # refer : http://gmt-tutorials.org/en/making_first_map.html
-gmt pscoast -R19.42/22.95/59.71/60.56 \
-  -JM15c \
-  -W0.1p,black \
-  -Dh \
-  -Scornflowerblue \
-  -Gdarkseagreen2 \
-  -P \
-  -Ba2f0.5g1 \
-  -BWSne+t"Archipelago Sea" \
-  -A0.2 \
-  > temp.ps
-open -a preview temp.ps
+#         http://www.matthewwherman.com/documents/tutorials/gmt_tutorial_1.pdf
+#         http://gmt.soest.hawaii.edu/doc/5.3.2/makecpt.html
+#         http://gmt.soest.hawaii.edu/doc/5.3.2/gmt.conf.html
+PROJ="-JQ80c"
+LIMS="-R-80/80/-90/90"
+PSFILE="earthquakes.ps"
+XOFFSET="-X2"
+YOFFSET="-Y4"
+ORIENTATION="-P"
+additional_parameters="--PS_MEDIA=100cx50c"
+
+gmt pscoast $PROJ $LIMS -W1p -Dh -N1/0.5p $XOFFSET $YOFFSET -K $ORIENTATION $additional_parameters > $PSFILE
+gmt psbasemap $PROJ $LIMS -Bxa20g10 -Bya30g5 -BWeSn \
+  -O -K $ORIENTATION $additional_parameters>> $PSFILE
+
+gmt psxy eqs.xy $PROJ $LIMS -Sc0.5c -W0.25p -Ccolors.cpt -O $ORIENTATION $additional_parameters >> $PSFILE
+
+open -a preview #PSFILE
